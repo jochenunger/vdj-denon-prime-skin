c8e33f18317b98a8857dd10586343cf34067d3f2
Jochen Unger <jochen.unger@googlemail.com>
2023-03-06 18:01:35 +0100

    Improvement of deployment script

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,9 @@
-New-Item -Name 'target' -ItemType 'directory' -Force;
-Copy-Item '.\Denon PRIME Screen.xml' -Destination '.\target\Denon Prime4 Screen.xml';
-Copy-Item '.\Denon PRIME Screen.png' -Destination '.\target\Denon Prime4 Screen.png';
-Copy-Item '.\Denon PRIME Screen.xml' -Destination '.\target\Denon PrimeGO Screen.xml';
-Copy-Item '.\Denon PRIME Screen.png' -Destination '.\target\Denon PrimeGO Screen.png';
-Compress-Archive -Path '.\target\Denon PRIMEGO Screen.*' -Update -DestinationPath '..\..\Denon PRIMEGO Screen.zip';
-Compress-Archive -Path '.\target\Denon Prime4 Screen.*' -Update -DestinationPath '..\..\Denon Prime4 Screen.zip';
-Compress-Archive -Path '.\target\Denon PRIMEGO Screen.*' -Update -DestinationPath '.\target\Denon PRIMEGO Screen.zip';
-Compress-Archive -Path '.\target\Denon Prime4 Screen.*' -Update -DestinationPath '.\target\Denon Prime4 Screen.zip';
+Remove-Item '.\target' -Recurse
+New-Item -Name '.\target' -ItemType 'directory'
+Copy-Item '.\Denon PRIME Screen.xml' -Destination '.\target\Denon Prime4 Screen.xml'
+Copy-Item '.\Denon PRIME Screen.png' -Destination '.\target\Denon Prime4 Screen.png'
+Copy-Item '.\Denon PRIME Screen.xml' -Destination '.\target\Denon PrimeGO Screen.xml'
+Copy-Item '.\Denon PRIME Screen.png' -Destination '.\target\Denon PrimeGO Screen.png'
+Compress-Archive -Path '.\target\Denon PRIMEGO Screen.*' -Update -DestinationPath '.\target\Denon PRIMEGO Screen.zip'
+Compress-Archive -Path '.\target\Denon Prime4 Screen.*' -Update -DestinationPath '.\target\Denon Prime4 Screen.zip'
+Copy-Item '.\target\Denon PRIME*.zip' -Destination '..\..\'
